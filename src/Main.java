b333c10d6f4332acbf6c953775350100fccfa677
eJwtzMGqwjAQheF3yboLW+I1zVIE3Sgidh2SyVRDYyck6a0ivrtBujsc+P430wCY0pUGHJlkG9OAELiuAYRpNe9XbYuIvNW1MNo2wK0xf9ywioVI/85iXOh7Kam8pG53Uqe8OdbnqevOx4Ozq9O2Ez1c4LrbH/J8adARv5fUj6j8ClicQR0xljcB/Q5tH26UYTLegRrwVUXUVlK8lRGomhJGiQ/tfDVTHHpPc8H4DC5iUjozOU7efz5fBIhQ9A== <ivushka777@mail.ru>
2023-11-28 13:37:18 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
+import animals.Animal;
+import data.AnimalData;
 import data.CommandsData;
+import factory.AnimalFactory;
 
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Animal> animalList = new ArrayList<>();
+
+        AnimalFactory animalFactory = new AnimalFactory();
 
         while(true) {
             System.out.println("Введите команду: add/list/exit");
-            String commandStr = scanner.next();
+            String commandStr = scanner.next().toUpperCase().trim();
 
             boolean isCommandsExist = false;
             for (CommandsData commandsData : CommandsData.values()) {
-                if (commandsData.name().equals(commandStr.toUpperCase().trim())) {
+                if (commandsData.name().equals(commandStr)) {
                     isCommandsExist = true;
                     break;
                 }
@@ -23,8 +28,30 @@ public class Main {
                 System.out.println("Введена некорректная команда");
                 continue;
             }
-            System.out.println("Good!");
+            CommandsData commandsData = CommandsData.valueOf(commandStr);
+            switch (commandsData){
+                case ADD:
+//                    System.out.println("Введите тип животного");
+//                    Animal animal = animalFactory.create(AnimalData.valueOf(scanner.next()));
+//                    System.out.println("Введите имя животного");
+//                    animal.setName (scanner.next());
+//                    System.out.println("Введите возраст животного");
 
+
+
+
+                String name = scanner.next();
+                String ageStr = scanner.next();
+                String weightStr = scanner.next();
+                String colorStr = scanner.next();
+
+
+                case LIST:
+                    for (Animal animal: animalList){}
+                    System.out.println(animalList.toString()); // animal.toString() подсвечивает ошибку тогда
+                case EXIT:
+                   System.exit(0);
+            }
         }
 
     }
